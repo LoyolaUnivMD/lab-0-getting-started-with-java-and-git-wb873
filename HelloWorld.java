5007b10cd9c0f660c084a84c84f61a59291e3de7
wilburgess <wbburgess@loyola.edu>
2024-01-23 11:25:46 -0500

    Finished Lab 0

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,30 @@
 //HelloWorld.java
 
+// Programmers:  Wil
+// Course:  CS 212, Mr. John
+// Due Date: 1/23/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: ASCII Art
+// Reminder: you should never take code from the Internet or another person
+
+
+
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+
+
+        System.out.println("        __        ______  ");
+        System.out.println("       \\ \\      / / __ )");
+        System.out.println("        \\ \\ /\\ / /|  _ \\");
+        System.out.println("         \\ V  V / | |_) |");
+        System.out.println("          \\_/\\_/  |____/");
+
+
+
         
     }
 }
